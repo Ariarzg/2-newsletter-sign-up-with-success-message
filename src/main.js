8f5166db579ce6f9a46a809c8fa8536281310308
Aria Rouzegar <ariarzg@gmail.com>
2025-07-26 02:45:21 +0330

    Fix V1

--- src/main.js
+++ src/main.js
@@ -12,7 +12,7 @@ document.getElementById('form').addEventListener('submit', (e) => {
   showEmailValidity();
 
   if (isEmailValid) {
-    window.location = '../pages/success.html';
+    window.location = '../pages';
   }
 });
 
