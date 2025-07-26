be70fc583dd2ed0bab303f06a425417d4889527b
Aria Rouzegar <ariarzg@gmail.com>
2025-07-26 04:16:44 +0330

    Netlify Fix Last Version

--- pages/success.js
+++ pages/success.js
@@ -2,7 +2,7 @@ import './success.scss';
 
 const email = localStorage.getItem('ariarzg-email-validated');
 document.getElementById('success').innerHTML = `
-  <img src="../assets/images/icon-success.svg" alt="success icon">
+  <img src="../public/icon-success.svg" alt="success icon">
   <h1>Thanks for subscribing!</h1>
   <p>A confirmation email has been sent to
   <span class="sent-to">${email}</span>.
