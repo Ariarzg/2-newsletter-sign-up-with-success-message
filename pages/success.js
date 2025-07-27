4f98a8bddc4372f8dad4563486e77a4a8a263c06
Aria Rouzegar <ariarzg@gmail.com>
2025-07-27 14:16:36 +0330

    Add Fluid Paragraph and Clean Up Some Code

--- pages/success.js
+++ pages/success.js
@@ -4,7 +4,7 @@ const email = localStorage.getItem('ariarzg-email-validated');
 document.getElementById('success').innerHTML = `
   <img src="../icon-success.svg" alt="success icon">
   <h1>Thanks for subscribing!</h1>
-  <p>A confirmation email has been sent to
+  <p class="confirmation-text">A confirmation email has been sent to
   <span class="sent-to">${email}</span>.
   Please open it and click the button inside to confirm your subscription.</p>
   <button class="main-button" id="dismiss-btn">Dismiss message</button>
